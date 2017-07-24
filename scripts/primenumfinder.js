9371a10bd0af703a6a1cca66f8c6fa207627c142
Edward Wong <wong.eddiek@gmail.com>
2017-07-24 08:59:45 -0600

    optimized function

--- scripts/primenumfinder.js
+++ scripts/primenumfinder.js
@@ -70,6 +70,22 @@ function primeList(max) {
   return primelist;
 }
 
+//generates a prime list containing n number of primes.
+function nPrimeList(n) {
+  var primelist = []
+  for (var i = 2; primelist.length < n; i++) {
+    if (isPrime(i)) {
+      primelist.push(i);
+    }
+  }
+  return primelist;
+}
+
+//return the nth prime starting at 2.
+function findNthPrime(n) {
+  return nPrimeList(n).pop();
+}
+
 //new prime list function using Sieve of Eratosthenes algorithm - efficient
 //can find primes from 1 - 100,000,000 in ~ 8.39s.
 function primeListNew(max) {
@@ -85,6 +101,35 @@ function primeListNew(max) {
   for (var i = 3; i < Math.sqrt(numList.length); i+=2) {
     for (var j = i; i * j < numList.length; j++) {
       numList[i * j] = false;
+    }
+  }
+  var primeList = []
+  for (var i = 2; i < numList.length; i++) {
+    if (numList[i]) {
+      primeList.push(i);
+    }
+  }
+  console.timeEnd();
+  return primeList;
+}
+
+//Optimized the SoE algorithm functio
+//can find primes from 1 - 100,000,000 in < 5s.
+function primeListNew2(max) {
+  console.time();
+  var numList = [];
+  for (var i = 0; i <= max; i++) {
+    numList.push(true);
+  }
+  //turn all even index to false
+  for (var i = 4; i < numList.length; i+=2) {
+    numList[i] = false;
+  }
+  for (var i = 3; i < Math.sqrt(numList.length); i+=2) {
+    if (numList[i] != false) {
+      for (var j = i; i * j < numList.length; j+=2) {
+        numList[i * j] = false;
+      }
     }
   }
   var primeList = []
